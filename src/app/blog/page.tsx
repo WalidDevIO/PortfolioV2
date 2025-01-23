f3a54e830174bfb8c92a3a7aae622a5573960939
WalidTechIO <walid.elouazizi29@gmail.com>
2025-01-23 22:50:22 +0100

    Couleurs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -11,10 +11,10 @@ export default async function Blog({ searchParams }: { searchParams?: { [key: st
             <h1 className="text-4xl font-bold mb-4">Blog</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {posts.map((post) => (
-                <Card key={post.id} className="p-4 rounded-lg shadow border-2 border-gray-200 dark:border-gray-800 bg-transparent">
+                <Card key={post.id} className="p-4 rounded-lg shadow border-2 border-dark dark:border-white bg-transparent">
                     <CardHeader>
-                        <CardTitle className="text-4xl font-bold mb-4 text-gray-900 dark:text-gray-100">{post.title}</CardTitle>
-                        <p className="text-gray-600 dark:text-gray-400 mb-4">Posté le {post.created_at ? new Date(post.created_at).toLocaleDateString("fr-FR") : ""}</p>
+                        <CardTitle className="text-4xl font-bold mb-4 text-dark dark:text-white">{post.title}</CardTitle>
+                        <p className="text-dark dark:text-white mb-4">Posté le {post.created_at ? new Date(post.created_at).toLocaleDateString("fr-FR") : ""}</p>
                         <a href={`/blog/${post.slug}`}>
                             <Button variant="outline">Lire la suite</Button>
                         </a>
