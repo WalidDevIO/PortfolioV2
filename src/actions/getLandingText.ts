ca14b22303db6e63eafb3d9d696f7db5ef43274d
WalidDevIO <walid.elouazizi29@gmail.com>
2025-09-23 20:33:26 +0200

    Plugins in getLandingText

--- src/actions/getLandingText.ts
+++ src/actions/getLandingText.ts
@@ -1,21 +1,41 @@
 import { createClientServer } from "@/lib/supabase";
 import { Converter } from "showdown"
 
+interface Plugin {
+    regex: RegExp,
+    plugin: (group: string) => string
+}
+
+const yearDiffPlugin = (group: string): string => {
+    const todayYear = new Date().getFullYear()
+    const year = parseInt(group.match(/[0-9]+/)![0])
+    return (todayYear - year).toString()
+}
+
+const PLUGINS: Plugin[] = [
+    {
+        regex: /{yearDiff\([0-9]+\)}/g,
+        plugin: yearDiffPlugin
+    }
+]
+
 export async function getLandingText(): Promise<string> {
     const supabase = createClientServer()
     const converter = new Converter()
 
-    const {data, error} = await supabase.from("configuration").select("landing_text,birth_year,exp_year").single()
+    const {data, error} = await supabase.from("configuration").select("landing_text").single()
 
     if(error) {
         throw error
     }
 
-    const todayYear = new Date().getFullYear()
+    return converter.makeHtml(preformat_text(data.landing_text))
+}
 
-    const preformatted_text = data.landing_text
-        .replace('{$myAge}', todayYear - data.birth_year)
-        .replace('{$myExperienceYears}', todayYear - data.exp_year)
-
-    return converter.makeHtml(preformatted_text)
+const preformat_text = (text: string) => {
+    let formatted_text = text
+    PLUGINS.forEach(({regex, plugin}) => {
+        formatted_text = formatted_text.replaceAll(regex, (match) => plugin(match))
+    })
+    return formatted_text
 }
