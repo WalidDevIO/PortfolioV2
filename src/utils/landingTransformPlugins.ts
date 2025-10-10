0aac2aaada5211db641562429f29edd23e67c30f
WalidDevIO <walid.elouazizi29@gmail.com>
2025-10-11 00:13:31 +0200

    Fix linter unused supabase var (tmp)

--- src/utils/landingTransformPlugins.ts
+++ src/utils/landingTransformPlugins.ts
@@ -6,6 +6,7 @@ export interface Plugin {
     transform: (match: string, ...args: any[]) => string
 }
 
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getPlugins = async (supabase: SupabaseClient): Promise<Plugin[]> => {
 
     return [
